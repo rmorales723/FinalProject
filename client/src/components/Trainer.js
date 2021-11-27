226741e97dfa83131039f07adc9d28bf6711cd58
robert morales <rmoralfit@gmail.com>
2021-11-27 13:44:03 -0500

    added routes / added trainer.js

---
+++ client/src/components/Trainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+function Trainer(props) {
+    const [name, setName] = useState("");
+    const [skill, setSkill] = useState("");
+    const [credential, setCredential] = useState("");
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+
+const 
+
+
+
+
+    export default Trainer;
