daec52f1e570740d122a774d82af522b58aa1229
robert morales <rmoralfit@gmail.com>
2021-12-18 20:47:43 -0500

    error handling in api call and fixed bugs

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,10 +1,10 @@
 class Appointment < ApplicationRecord
     belongs_to :trainer
     belongs_to :client
+    validates :client_name, :date, :time, presence: true
 
-    validates :date, :time, presence: true
-
-    def client_name 
+    def client_name
+        # client&.name
         client.name
     end
 end
--- client/src/components/AppointmentsContainer.js
+++ client/src/components/AppointmentsContainer.js
@@ -6,14 +6,25 @@ function AppointmentsContainer() {
     const [appointments, setAppointments] = useState([])
 
     useEffect(() => {
-        fetch("/appointments")
-        .then((res) => res.json())
-        .then((data) => setAppointments(data))
-    },[]) 
+        try {
+            fetch("/appointments").then((response) => {
+                if (response.ok) {
+                  return response.json();
+                } else {
+                  throw new Error('Something went wrong. Please check your rails server log!');
+                }
+              }).then((data) => setAppointments(data))
+          } catch (e) {
+            alert(e);
+          }
+    },[])
+
+
 
     const appointmentsToRender = () => {
+        // const appointmentsArray = Array.from(appointments)
         return appointments.map((appointment) => <Appointment id={appointment.id} client_name = {appointment.client_name} date = {appointment.date} time = {appointment.time} appointments={appointments} setAppointments={setAppointments} /> )
-    }
+        }
 
     return(
         <div className="body-app">
