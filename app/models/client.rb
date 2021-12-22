09319f02243b359d52765247956b6850ce68199c
robert morales <rmoralfit@gmail.com>
2021-12-21 21:50:14 -0500

    email format validation with regex

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ApplicationRecord
     has_many :appointments, dependent: :destroy
     validates_presence_of :name, :number, :img_url, :email
+
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
