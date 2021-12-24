43c0dac430e94a8b53201cecdc81c2c1804463f7
robert morales <rmoralfit@gmail.com>
2021-12-23 21:34:46 -0500

    Reactored clients page with a new Client component

---
+++ client/src/components/Client.js
@@ -0,0 +1,65 @@
+import { Button, Card } from 'react-bootstrap';
+import { useState, useEffect} from 'react';
+import { useHistory, useParams } from 'react-router';
+import EditAppointment from './EditAppointment';
+import { Link } from "react-router-dom";
+import '../App.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import StarRating from './StarRating';
+
+const Client = ({ id, name, number, email, rating, img_url, clients, setClients }) => {
+
+const deleteClient = (event) => {
+    fetch(`/clients/${event.target.id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(() => {
+        const filterClients = clients.filter(client => {
+            return client.id != event.target.id;
+        })
+        setClients(filterClients);
+        // history.push('/clients');
+    });
+}
+
+const styles = {
+    cardImage: {
+        objectFit: 'cover',
+        borderRadius: 55,
+        width: '100%',
+        height: '100%'
+    }
+  }
+
+const history = useHistory();
+
+const editAppointment = (event) => {
+    history.push(`/appointments/${id}/edit`);
+}
+    return(
+        <div className="card-box">
+        <Card className="card-size" style={{ width: '18rem' }}>
+            <Card.Img style={styles.cardImage} className="img-url-size" variant="top" src={img_url} />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{number}</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">{email}</Card.Subtitle>
+                <Card.Text><StarRating totalStars={5} currentRating={rating} displayStar={true} /></Card.Text>
+                <div className="button-organizer">
+                    <div className="d-grid gap-2">
+                        <Link className="d-grid gap-2" to={`/clients/${id}/edit`}>
+                            <Button variant="primary" size="sm" >EDIT</Button>
+                        </Link>
+                        <Button id={id} className="gap-2" variant="danger" size="sm" onClick={deleteClient}>DELETE</Button>
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+    </div>      
+    )
+  }
+
+export default Client;
--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
-import StarRating from './StarRating';
+import Client from './Client';
 
 function Clients() {
     const [clients, setClients] = useState([])
@@ -14,54 +14,8 @@ function Clients() {
             .then((clients) => setClients(clients))
     }, [])
 
-    const deleteClient = (event) => {
-        fetch(`/clients/${event.target.id}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }).then(() => {
-            const filterClients = clients.filter(client => {
-                return client.id != event.target.id;
-            })
-            setClients(filterClients);
-            // history.push('/clients');
-        });
-    }
-
-    const styles = {
-        cardImage: {
-            objectFit: 'cover',
-            borderRadius: 55,
-            width: '100%',
-            height: '100%'
-        }
-      }
-
     const renderClients = () => {
-        return clients.map((client) => {
-            return (
-                <div className="card-box">
-                    <Card className="card-size" style={{ width: '18rem' }}>
-                        <Card.Img style={styles.cardImage} className="img-url-size" variant="top" src={client.img_url} />
-                        <Card.Body>
-                            <Card.Title>{client.name}</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">{client.number}</Card.Subtitle>
-                            <Card.Subtitle className="mb-2 text-muted">{client.email}</Card.Subtitle>
-                            <Card.Text><StarRating totalStars={5} currentRating={client.rating} displayStar={true} /></Card.Text>
-                            <div className="button-organizer">
-                                <div className="d-grid gap-2">
-                                    <Link className="d-grid gap-2" to={`/clients/${client.id}/edit`}>
-                                        <Button variant="primary" size="sm" >EDIT</Button>
-                                    </Link>
-                                    <Button id={client.id} className="gap-2" variant="danger" size="sm" onClick={deleteClient}>DELETE</Button>
-                                </div>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </div>
-            )
-        });
+        return clients.map((client) => <Client id={client.id} name={client.name} number={client.number} rating={client.rating} email={client.email} img_url={client.img_url} clients={clients} setClients={setClients} /> );
     };
 
     return (
