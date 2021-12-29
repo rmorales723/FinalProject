ee8aaf73847dd672754f59e19823c4e0a937e110
robert morales <rmoralfit@gmail.com>
2021-12-28 21:31:07 -0500

    Fixed seeds file

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -3,5 +3,4 @@ class Trainer < ApplicationRecord
     has_many :appointments
     has_many :clients, through: :appointments
     validates :username, presence: true, uniqueness: {case_sensitive: false}
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ Trainer.destroy_all
 Client.destroy_all
 Appointment.destroy_all
 
-Trainer.create!(name: "Rob", username: "rob", password: "password", skill: "Fat Loss, Body Building, Cross Training, Contest Prep", credential: "National Strength & Conditiong Association, USA Weightlifting, KettleBell Concepts, CPR")
-Client.create!(name:"Sarah", number:"917-386-6955", img_url: "https://publicdomainvectors.org/photos/1389952697.png")
-Appointment.create!(client_name:"Sarah", date:"12-30-2021", time: "10:00")
+trainer = Trainer.create!(name: "Rob", username: "rob", password: "password", skill: "Fat Loss, Body Building, Cross Training, Contest Prep", credential: "National Strength & Conditiong Association, USA Weightlifting, KettleBell Concepts, CPR")
+client = Client.create!(name:"Sarah", email: 'sarah@email.com', number:"917-386-6955", img_url: "https://publicdomainvectors.org/photos/1389952697.png")
+Appointment.create!(client: client, trainer: trainer, date:"2021-12-31", time: "10:00am")
