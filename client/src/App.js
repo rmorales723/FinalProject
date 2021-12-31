7a4f0506a6c16e7cf995108cfde2d9245767927b
robert morales <rmoralfit@gmail.com>
2021-12-30 20:15:32 -0500

    trying to fix bootstrap styles in heroku

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import AppointmentForm from './components/AppointmentForm'
 import AppointmentsContainer from './components/AppointmentsContainer'
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-calendar/dist/Calendar.css';
 
 function App() {
