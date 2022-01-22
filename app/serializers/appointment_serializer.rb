b1984c34e0e4cce0b556a9e9790cac76dd3c04ce
robert morales <rmoralfit@gmail.com>
2022-01-21 20:59:36 -0500

    single calendar view working with multiple appointments, showing them in correct time slots

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,7 +1,21 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :date, :time, :client_name, :trainer_id, :client_id
+  attributes :id, :date, :time, :client_name, :trainer_id, :client_id, :start, :end, :title  
 
   def date
     Date.parse(object.date).strftime("%Y-%m-%d")
   end
+
+  def start    
+    DateTime.parse(Date.parse(object.date).strftime("%Y-%m-%d") + " #{object.time}").strftime("%Y-%m-%d %H:%M")
+  end
+
+  def end    
+    (DateTime.parse(start) + 1.hour).strftime("%Y-%m-%d %H:%M")
+  end
+
+  def title
+    "Appointment with #{object.client_name}"
+  end
 end
+
+
--- client/src/components/AppointmentsContainer.js
+++ client/src/components/AppointmentsContainer.js
@@ -1,11 +1,35 @@
 import React,{useEffect, useState} from 'react';
 import Appointment from './Appointment';
 import { Button, Card } from 'react-bootstrap';
-import Calendar from 'react-calendar';
+// import Calendar from 'react-calendar';
+import { Calendar, momentLocalizer, dateFnsLocalizer, Event} from 'react-big-calendar';
+import withDragAndDrop, { withDragAndDropProps } from 'react-big-calendar/lib/addons/dragAndDrop';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from 'moment';
+import startOfWeek from 'date-fns/startOfWeek'
+import enUS from 'date-fns/locale/en-US'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import getDay from 'date-fns/getDay'
 
 function AppointmentsContainer() {
     const [appointments, setAppointments] = useState([])
-    const [appointmentDates, setAppointmentDates] = useState([])
+    const [selectedEvent, setSelectedEvent] = useState(undefined)
+    const [modalState, setModalState] = useState(false)
+ 
+    const handleSelectedEvent = (event) => {
+       setSelectedEvent(event)
+       setModalState(true)
+    }
+
+    const Modal = () => {
+      return (
+         <div className={`modal-${modalState == true ? 'show' : 'hide'}`}>
+           {selectedEvent.title}
+           Show the links!
+         </div>
+      )
+    }
 
     useEffect(() => {
         try {
@@ -25,26 +49,50 @@ function AppointmentsContainer() {
       return <div>Loading...</div>
     }
 
-    function appointmentDatess() {
-      // console.log('appointments', appointments);
-      // let apps = appointments.map(app => new Date(app.date))
-      const apps = appointments.map((app) => {
-        // console.log('app.date', app.date);
-        return new Date(app.date);
-      })
-      // console.log('apps', apps);
-      setAppointmentDates(apps);
-      // return apps;
+    const locales = {
+      'en-US': enUS,
     }
 
-    // appointmentDatess();
+    const localizer = dateFnsLocalizer({
+      format,
+      parse,
+      startOfWeek,
+      getDay,
+      locales,
+    })
+
+    const start = new Date('2022-01-21 19:00')
+    const end = new Date('2022-01-21 20:00')
+
+    const myEventsList = appointments.map((app) => {
+      return {
+        title: app.title,
+        start: new Date(app.start),
+        end: new Date(app.end)
+      };
+    })   
+
+    const MyCalendar = props => (
+      <div>
+        {selectedEvent && <Modal />}
+        <Calendar
+          defaultView='week'
+          events={myEventsList}
+          localizer={localizer}                    
+          resizable
+          allDayAccessor={false}
+          onSelectEvent={(e) => handleSelectedEvent(e)}
+          style={{ height: '100vh' }}
+        />
+      </div>
+    )
 
     const appointmentsToRender = () => {      
       // console.log('appointmentDates', appointmentDates);        
         // return appointments.map((appointment) => <Appointment id={appointment.id} client_name = {appointment.client_name} date = {appointment.date} time = {appointment.time} appointments={appointments} setAppointments={setAppointments} /> )
         // return <Calendar value={appointmentDates} />
         // return <Calendar defaultValue={[appointments[0].date, appointments[1].date, appointments[2].date]} />
-        return <Calendar value={['2017-01-01', '2017-08-01', appointments[0].date]} />
+        return <MyCalendar />
         // return <Calendar value={[new Date(appointments[2].date), new Date(appointments[1].date)]} />
     }
 
