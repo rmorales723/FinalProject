1da432a4cadce01f4f043843eed946ac24aea4be
robert morales <rmoralfit@gmail.com>
2022-01-22 20:41:07 -0500

    cleanup unnecessary code and debugging info

--- client/src/components/AppointmentsContainer.js
+++ client/src/components/AppointmentsContainer.js
@@ -1,38 +1,21 @@
 import React,{useEffect, useState} from 'react';
-import Appointment from './Appointment';
-import { Button, Card } from 'react-bootstrap';
-// import Calendar from 'react-calendar';
-import { Calendar, momentLocalizer, dateFnsLocalizer, Event} from 'react-big-calendar';
-import withDragAndDrop, { withDragAndDropProps } from 'react-big-calendar/lib/addons/dragAndDrop';
+import { Card } from 'react-bootstrap';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import moment from 'moment';
-import startOfWeek from 'date-fns/startOfWeek'
-import enUS from 'date-fns/locale/en-US'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import getDay from 'date-fns/getDay'
+import startOfWeek from 'date-fns/startOfWeek';
+import enUS from 'date-fns/locale/en-US';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import getDay from 'date-fns/getDay';
 import { useHistory } from 'react-router';
 
 function AppointmentsContainer() {
     const [appointments, setAppointments] = useState([])
-    const [selectedEvent, setSelectedEvent] = useState(undefined)
-    const [modalState, setModalState] = useState(false)
 
     const history = useHistory();
- 
-    const handleSelectedEvent = (event) => {
-       setSelectedEvent(event)
-       setModalState(true)
-       history.push(`/appointments/${event.id}/edit`);
-    }
 
-    const Modal = () => {
-      return (
-         <div className={`modal-${modalState == true ? 'show' : 'hide'}`}>
-           {selectedEvent.title}
-           Show the links!
-         </div>
-      )
+    const handleSelectedEvent = (event) => {
+       history.push(`/appointments/${event.id}/edit`);
     }
 
     useEffect(() => {
@@ -72,30 +55,18 @@ function AppointmentsContainer() {
         start: new Date(app.start),
         end: new Date(app.end)
       };
-    })   
+    })
 
-    const MyCalendar = props => (
-      <div>
-        {selectedEvent && <Modal />}
-        <Calendar
-          defaultView='week'
-          events={myEventsList}
-          localizer={localizer}                    
-          resizable
-          allDayAccessor={false}
-          onSelectEvent={(e) => handleSelectedEvent(e)}
-          style={{ height: '100vh' }}
-        />
-      </div>
-    )
-
-    const appointmentsToRender = () => {      
-      // console.log('appointmentDates', appointmentDates);        
-        // return appointments.map((appointment) => <Appointment id={appointment.id} client_name = {appointment.client_name} date = {appointment.date} time = {appointment.time} appointments={appointments} setAppointments={setAppointments} /> )
-        // return <Calendar value={appointmentDates} />
-        // return <Calendar defaultValue={[appointments[0].date, appointments[1].date, appointments[2].date]} />
-        return <MyCalendar />
-        // return <Calendar value={[new Date(appointments[2].date), new Date(appointments[1].date)]} />
+    const appointmentsToRender = () => {
+      return <Calendar
+        defaultView='week'
+        events={myEventsList}
+        localizer={localizer}
+        resizable
+        allDayAccessor={false}
+        onSelectEvent={(e) => handleSelectedEvent(e)}
+        style={{ height: '100vh' }}
+      />
     }
 
     return(
@@ -103,7 +74,7 @@ function AppointmentsContainer() {
             backgroundImage:  `url(${process.env.PUBLIC_URL + '/images/gym-appointment.jpeg'})`,
             backgroundRepeat: 'no-repeat', backgroundSize: `cover`,
           }} className="body-app">
-            <Card className="card-size">                
+            <Card className="card-size">
                 <Card.Body>
                     <Card.Title>ALL APPOINTMENTS</Card.Title>
                     {appointmentsToRender()}
